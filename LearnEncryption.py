20e238400f4ef7a5d77132ed87f894ecb70a6f89
Abhishek Mishra <abhirules27@gmail.com>
2022-08-11 22:58:32 +0530

    Started Coding https://www.youtube.com/watch?v=ZpMABSUwNj8&list=PL8KnQ7ULK8egs86oy1gRRa21CGDrEefPw&index=9

--- LearnEncryption.py
+++ LearnEncryption.py
@@ -1,1 +1,14 @@
 # Generating Character for Encryption
+
+import random
+msg = 'Hello World'
+key = ''
+encryption_level = 128 // 8
+char_pool = ''
+for i in range(0x00, 0x255):
+    char_pool += chr(i)
+# print(char_pool)
+# print(msg)
+key = ''
+for i in range(encryption_level):
+    key += random.choice(char_pool)
