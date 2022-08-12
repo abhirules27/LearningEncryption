b215d099137a39506e2b1415d310a21c530f2244
Abhishek Mishra <abhirules27@gmail.com>
2022-08-12 17:16:56 +0530

    Stage 1

--- LearnEncryption.py
+++ LearnEncryption.py
@@ -12,3 +12,19 @@ for i in range(0x00, 0x255):
 key = ''
 for i in range(encryption_level):
     key += random.choice(char_pool)
+#print(key)
+
+key_index = 0
+max_key_index = encryption_level - 1
+encrypted_msg = ''
+for char in msg:
+    encrypted_char = ord(char) ^ ord(key[key_index])
+    encrypted_msg += chr(encrypted_char)
+
+    # Checking Index Status
+    if key_index >= max_key_index:
+        key_index = 0
+    else:
+        key_index += 1
+print(msg)
+print(encrypted_msg)
