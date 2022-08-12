d2da40850be3967c23aadbe5ff526c7934bac3cf
Abhishek Mishra <abhirules27@gmail.com>
2022-08-12 17:42:20 +0530

    Stage 1: Generating Key Stage 2: Encryptng Message Stage 3: Decrypting Encrypted Msg of Stage 2

--- LearnEncryption.py
+++ LearnEncryption.py
@@ -1,7 +1,10 @@
 # Generating Character for Encryption
 
 import random
-msg = 'Hello World'
+msg = 'Hello World. How are you doing!'
+
+# Stage 1: Generating Key
+
 key = ''
 encryption_level = 128 // 8
 char_pool = ''
@@ -14,11 +17,13 @@ for i in range(encryption_level):
     key += random.choice(char_pool)
 #print(key)
 
-key_index = 0
-max_key_index = encryption_level - 1
-encrypted_msg = ''
+# Stage 2: Checking for Msg len vs Key len Cycle
+
+key_index = 0                           # Current Cycle
+max_key_index = encryption_level - 1    # 15th Cycle
+encrypted_msg = ''                      # Blank Encrypted Msg
 for char in msg:
-    encrypted_char = ord(char) ^ ord(key[key_index])
+    encrypted_char = ord(char) ^ ord(key[key_index])    # Ord converts Char to ASCII
     encrypted_msg += chr(encrypted_char)
 
     # Checking Index Status
@@ -26,5 +31,19 @@ for char in msg:
         key_index = 0
     else:
         key_index += 1
-print(msg)
-print(encrypted_msg)
+print(f'\nOriginal Message : {msg}')
+print(f'Encrypted Message: {encrypted_msg}')
+
+# Stage 3: Decrypting Message
+key_index = 0
+decrypted_msg = ''
+for char in encrypted_msg:
+    decrypted_char = ord(char) ^ ord(key[key_index])
+    decrypted_msg += chr(decrypted_char)
+
+    # Checking Index Status
+    if key_index >= max_key_index:
+        key_index = 0
+    else:
+        key_index += 1
+print(f'Decrypted Message: {decrypted_msg}')
